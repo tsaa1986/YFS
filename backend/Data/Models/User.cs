511b0b0f54cc705d893e72020a225bf971582ebf
Anton Tsymbal <Tsymbal.Anton@gmail.com>
2022-11-09 11:16:42 +0200

    changes user model

--- backend/Data/Models/User.cs
+++ backend/Data/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -12,5 +13,7 @@ namespace YFS.Data.Models
         public DateTime CreatedOn { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+
+        public List<AccountGroup> AccountsGroup { get; set; }
     }
 }
