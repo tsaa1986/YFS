a5081a8394ba00d75e65f1d6332aae02ac445972
Anton Tsymbal <Tsymbal.Anton@gmail.com>
2022-11-09 11:17:48 +0200

    add seed data files

---
+++ backend/Data/Models/IdentitySeedData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace YFS.Data.Models
+{
+    public static class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Secret123$";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            /*UserManager<User> userManager = app.ApplicationServices
+            .GetRequiredService<UserManager<User>>();
+            IdentityUser user = await userManager.FindByIdAsync(adminUser);
+            if (user == null)
+            {
+               // user = new User("Admin");
+               // await userManager.CreateAsync(user, adminPassword);
+            }*/
+        }
+    }
+}
---
+++ backend/Data/Models/SeedData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using YFS.Data.Repository;
+
+namespace YFS.Data.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IServiceProvider services)
+        {
+            RepositoryContext context =
+            services.GetRequiredService<RepositoryContext>();
+            //context.Database.Migrate();
+            //if (!context.Products.Any())
+            //{
+            //    context.Products.AddRange(
+            // ...statements omiited for brevity...
+            //    );
+            //    context.SaveChanges();
+            //}
+        }
+    }
+}
