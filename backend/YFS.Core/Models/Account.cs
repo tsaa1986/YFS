fb80e78b41df2d0d4eaa2b0549060ac07b8f9cbe
Anton Tsymbal <Tsymbal.Anton@gmail.com>
2022-11-18 10:27:48 +0200

    add fields account model

--- backend/YFS.Core/Models/Account.cs
+++ backend/YFS.Core/Models/Account.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace YFS.Core.Models
@@ -8,15 +9,24 @@ namespace YFS.Core.Models
         [Column("AccountId")]
         public int Id { get; set; }
 
-        [Column(TypeName = "VARCHAR")]
-        [Required(ErrorMessage = "Acount's name is a required field")]
-        [MaxLength(255, ErrorMessage = "Maximum length for the Name is 255 characters.")]
-        public string? Name { get; set; }
-
         [ForeignKey(nameof(AccountGroup))]
         public int AccountGroupId { get; set; }
 
         [ForeignKey(nameof(Currency))]
         public int CurrencyId { get; set; }
+        public int BankId { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [Required(ErrorMessage = "Acount's name is a required field")]
+        [MaxLength(255, ErrorMessage = "Maximum length for the Name is 255 characters.")]
+        public string? Name { get; set; }
+        public DateTime OpeningDate { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [MaxLength(255, ErrorMessage = "Maximum length for the Name is 255 characters.")]
+        public string? Note { get; set; }
+
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Balance { get; set; }
     }
 }
