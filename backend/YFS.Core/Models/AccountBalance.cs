ca956a0fa81baa29420254bf9f96e237c7e34e89
Ton@ <alwaysimproveyourself1@gmail.com>
2023-06-08 09:29:59 +0300

    fix bug accountBalance model. error durring remove operation-incorrect json

--- backend/YFS.Core/Models/AccountBalance.cs
+++ backend/YFS.Core/Models/AccountBalance.cs
@@ -14,7 +14,7 @@ namespace YFS.Core.Models
         public int Id { get; set; }
         [ForeignKey("Account")]
         public int AccountId { get; set; }
-        public virtual Account? Account { get; set; }
+        //public virtual Account? Account { get; set; }
         [Required]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Balance { get; set; }
