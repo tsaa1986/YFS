331b9a3596e815a5e96b374533a8f8f6ee5d6a74
Anton Tsymbal <Tsymbal.Anton@gmail.com>
2023-07-10 11:50:15 +0300

    add integration test get currencie from api

--- backend/YFS.IntegrationTests/CurrencyControllerIntegrationTests.cs
+++ backend/YFS.IntegrationTests/CurrencyControllerIntegrationTests.cs
@@ -1,4 +1,6 @@
+using Newtonsoft.Json;
 using System.Net;
+using YFS.Core.Dtos;
 
 namespace YFS.IntegrationTests
 {
@@ -10,17 +12,37 @@ namespace YFS.IntegrationTests
             => _client = factory.CreateClient(); 
 
         [Fact]
-        public async Task Get_ReturnsCurrencies()
+        public async Task Get_Returns_Currencies()
         {
             //Arrange
+            //contain in CurrencyData
 
-            
             //Act
             var response = await _client.GetAsync("/api/Currency");
+            var content = await response.Content.ReadAsStringAsync();
+            var currencies = JsonConvert.DeserializeObject<CurrencyDto[]>(content);
 
+            var currencyUSDDto = currencies.Where(a => a.CurrencyId.Equals(840));//currencies.FirstOrDefault();
+            var currencyUAHDto = currencies.Where(a => a.CurrencyId.Equals(980));
+            var currencyEuroDto = currencies.Where(a => a.CurrencyId.Equals(978));
 
             //Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            Assert.NotNull(currencyUSDDto.FirstOrDefault());
+            //Assert.Equal("840", currencyUSDDto.First().CurrencyId.ToString());
+            Assert.NotNull(currencyUAHDto.FirstOrDefault());
+            Assert.NotNull(currencyEuroDto.FirstOrDefault());
+
+            /*
+            // Alternatively, you can assert the entire object using the 'Assert.Equal' method
+            var expectedCurrencyDto = new CurrencyDto
+            {
+                CurrencyCode = "USD",
+                CurrencyName = "United States Dollar",
+                // Set other expected property values
+            };
+            */
         }
     }
 }
