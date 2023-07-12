9aed4aec788bb40e0ff81d72c95ab73524224d21
Ton@ <alwaysimproveyourself1@gmail.com>
2023-07-12 10:36:48 +0300

    add login as demo user with save jwt. dd static variable jwt token.add procedure getJwtToken

--- backend/YFS.IntegrationTests/TestingWebAppFactory.cs
+++ backend/YFS.IntegrationTests/TestingWebAppFactory.cs
@@ -2,12 +2,50 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+using System.Text;
 using YFS.Repo.Data;
 
 namespace YFS.IntegrationTests
 {
-    public class TestingWebAppFactory<TStartup> : WebApplicationFactory<Program> //where TEntryPoint : Program
+    public class TestingWebAppFactory<TStartup> : WebApplicationFactory<Program>
     {
+        private static string? _jwtTokenForDemoUser;
+        public TestingWebAppFactory()
+        {
+            if (_jwtTokenForDemoUser == null)
+            {
+                _jwtTokenForDemoUser = GetJwtTokenForDemoUserAsync().GetAwaiter().GetResult();
+            }
+        }
+        public static string GetJwtTokenForDemoUser()
+        {
+            return _jwtTokenForDemoUser;
+        }
+        private async Task<string> GetJwtTokenForDemoUserAsync()
+        {
+            // Create a HttpClient to make the request
+            var client = CreateClient();
+
+            // Prepare the request payload (if required)
+            var requestContent = new StringContent(
+                "{ \"username\": \"Demo\", \"password\": \"123$qweR\" }",
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            // Make the request to the authentication endpoint
+            var response = await client.PostAsync("/api/Authentication/sign-in", requestContent);
+
+            // Ensure the request was successful
+            response.EnsureSuccessStatusCode();
+
+            // Extract the JWT token from the response
+            var content = await response.Content.ReadAsStringAsync();
+            var token = JObject.Parse(content)["token"].ToString();
+
+            return token;
+        }
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             //builder.UseContentRoot(".");
