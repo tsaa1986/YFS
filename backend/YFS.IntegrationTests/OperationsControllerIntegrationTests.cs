45a3a6ec28152e10ed9d9310b3469fe8e512f954
Ton@ <alwaysimproveyourself1@gmail.com>
2023-07-14 14:23:16 +0300

    add integration test for getoperationforperiod

--- backend/YFS.IntegrationTests/OperationsControllerIntegrationTests.cs
+++ backend/YFS.IntegrationTests/OperationsControllerIntegrationTests.cs
@@ -1,5 +1,6 @@
 ﻿using Azure;
 using Azure.Core;
+using Microsoft.Identity.Client;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -9,11 +10,14 @@ using System.Net.Http.Headers;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit.Priority;
 using YFS.Core.Dtos;
+using YFS.Core.Models;
 
 namespace YFS.IntegrationTests
 {
     [Collection("IntegrationTests")]
+    [TestCaseOrderer(PriorityOrderer.Name, PriorityOrderer.Assembly)]
     public class OperationsControllerIntegrationTests
     {
         private readonly HttpClient _client;
@@ -24,28 +28,19 @@ namespace YFS.IntegrationTests
             _factory = factory;
             _client = _factory.CreateClient();
         }
-
-        [Fact]
-        public async Task Get_Last10OperationsAccount_Returns_Success()
+        //create 3 operation. per 100 000
+        private async Task CreateOperation3monthAutomatically(int _accountId)
         {
-            //Arrange
-            int accountId = 1;
-            var requestOperation = new HttpRequestMessage(HttpMethod.Get, $"/api/operations/last10/{accountId}");
-            requestOperation.Headers.Authorization = new AuthenticationHeaderValue("Bearer", TestingWebAppFactory<Program>.GetJwtTokenForDemoUser());
             var createOperation1Request = new HttpRequestMessage(HttpMethod.Post, "/api/Operations/0");
             createOperation1Request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", TestingWebAppFactory<Program>.GetJwtTokenForDemoUser());
-            var createOperation2Request = new HttpRequestMessage(HttpMethod.Post, "/api/Operations/0");
-            createOperation2Request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", TestingWebAppFactory<Program>.GetJwtTokenForDemoUser());
-            var createOperation3Request = new HttpRequestMessage(HttpMethod.Post, "/api/Operations/0");
-            createOperation3Request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", TestingWebAppFactory<Program>.GetJwtTokenForDemoUser());
 
-            //add 3 operations automatically with other date month
+            // Create operation 1
             var createOperation1Body = new
             {
                 transferOperationId = 0,
                 categoryId = 2,
                 typeOperation = 2,
-                accountId = accountId,
+                accountId = _accountId,
                 operationCurrencyId = 980,
                 currencyAmount = 100000,
                 operationAmount = 100000,
@@ -53,12 +48,22 @@ namespace YFS.IntegrationTests
                 description = "description operation 1",
                 tag = "tag operation 1"
             };
+
+            var createOperation1RequestBody = JsonConvert.SerializeObject(createOperation1Body);
+            createOperation1Request.Content = new StringContent(createOperation1RequestBody, Encoding.UTF8, "application/json");
+            var createOperation1Response = await _client.SendAsync(createOperation1Request);
+            createOperation1Response.EnsureSuccessStatusCode();
+
+            // Create operation 2
+            var createOperation2Request = new HttpRequestMessage(HttpMethod.Post, "/api/Operations/0");
+            createOperation2Request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", TestingWebAppFactory<Program>.GetJwtTokenForDemoUser());
+
             var createOperation2Body = new
             {
                 transferOperationId = 0,
                 categoryId = 2,
                 typeOperation = 2,
-                accountId = accountId,
+                accountId = _accountId,
                 operationCurrencyId = 980,
                 currencyAmount = 100000,
                 operationAmount = 100000,
@@ -66,12 +71,22 @@ namespace YFS.IntegrationTests
                 description = "description operation 2",
                 tag = "tag operation 2"
             };
+
+            var createOperation2RequestBody = JsonConvert.SerializeObject(createOperation2Body);
+            createOperation2Request.Content = new StringContent(createOperation2RequestBody, Encoding.UTF8, "application/json");
+            var createOperation2Response = await _client.SendAsync(createOperation2Request);
+            createOperation2Response.EnsureSuccessStatusCode();
+
+            // Create operation 3
+            var createOperation3Request = new HttpRequestMessage(HttpMethod.Post, "/api/Operations/0");
+            createOperation3Request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", TestingWebAppFactory<Program>.GetJwtTokenForDemoUser());
+
             var createOperation3Body = new
             {
                 transferOperationId = 0,
                 categoryId = 2,
                 typeOperation = 2,
-                accountId = accountId,
+                accountId = _accountId,
                 operationCurrencyId = 980,
                 currencyAmount = 100000,
                 operationAmount = 100000,
@@ -80,20 +95,20 @@ namespace YFS.IntegrationTests
                 tag = "tag operation 3"
             };
 
-            var createOperation1RequestBody = JsonConvert.SerializeObject(createOperation1Body);
-            createOperation1Request.Content = new StringContent(createOperation1RequestBody, Encoding.UTF8, "application/json");
-            var createOperation1Response = await _client.SendAsync(createOperation1Request);
-            createOperation1Response.EnsureSuccessStatusCode();
-
-            var createOperation2RequestBody = JsonConvert.SerializeObject(createOperation2Body);
-            createOperation2Request.Content = new StringContent(createOperation2RequestBody, Encoding.UTF8, "application/json");
-            var createOperation2Response = await _client.SendAsync(createOperation2Request);
-            createOperation2Response.EnsureSuccessStatusCode();
-
             var createOperation3RequestBody = JsonConvert.SerializeObject(createOperation3Body);
             createOperation3Request.Content = new StringContent(createOperation3RequestBody, Encoding.UTF8, "application/json");
             var createOperation3Response = await _client.SendAsync(createOperation3Request);
             createOperation3Response.EnsureSuccessStatusCode();
+        }
+
+        [Fact, Priority(1)]
+        public async Task Get_Last10OperationsAccount_Returns_Success()
+        {
+            //Arrange
+            int _accountId = 1;
+            var requestOperation = new HttpRequestMessage(HttpMethod.Get, $"/api/operations/last10/{_accountId}");
+            requestOperation.Headers.Authorization = new AuthenticationHeaderValue("Bearer", TestingWebAppFactory<Program>.GetJwtTokenForDemoUser());
+            await CreateOperation3monthAutomatically(_accountId);
 
             //Act
             var responseOperation = await _client.SendAsync(requestOperation);
@@ -103,6 +118,30 @@ namespace YFS.IntegrationTests
             //Assert
             Assert.Equal(HttpStatusCode.OK, responseOperation.StatusCode);
             Assert.True(operations.Length == 3);
+            Assert.True(operations[0].Balance == 300000);
+        }
+
+        [Fact, Priority(2)]
+        public async Task Get_LastOperationForPeriod_Returns_Success()
+        {
+            //Arrange
+            int _accountId = 1;
+            String startDate = DateTime.Now.AddMonths(-3).ToString("yyyy-MM-dd") + " 00:00:00";
+            String endDate = DateTime.Now.ToString("yyyy-MM-dd") + " 23:59:59";
+            //2023 - 04 - 01 / 2023 - 04 - 28
+            var requestOperation = new HttpRequestMessage(HttpMethod.Get, $"/api/Operations/period/{_accountId}/{startDate}/{endDate}");
+            requestOperation.Headers.Authorization = new AuthenticationHeaderValue("Bearer", TestingWebAppFactory<Program>.GetJwtTokenForDemoUser());
+            await CreateOperation3monthAutomatically(_accountId);
+
+            //Act
+            var responseOperation = await _client.SendAsync(requestOperation);
+            var contentOperation = await responseOperation.Content.ReadAsStringAsync();
+            var operations = JsonConvert.DeserializeObject<OperationDto[]>(contentOperation);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, responseOperation.StatusCode);
+            Assert.True(operations.Length == 6);
+            Assert.True(operations[0].Balance == 600000);
         }
     }
 }
