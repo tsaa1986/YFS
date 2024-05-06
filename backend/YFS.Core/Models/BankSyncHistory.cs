fb174a49b6105d6f5a3d6c0a8944180e0c3ded19
Anton Tsymbal <Tsymbal.Anton@gmail.com>
2024-05-06 10:58:38 +0300

    some changes in banksyncrepository

--- backend/YFS.Core/Models/BankSyncHistory.cs
+++ backend/YFS.Core/Models/BankSyncHistory.cs
@@ -10,7 +10,5 @@ namespace YFS.Core.Models
         public int Id { get; set; }
         public DateTime LastUpdated { get; set; }
         public string Country { get; set; }  // Added to specify the country of the synchronization
-
-
     }
 }
